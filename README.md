5f0a5052836322fb9d68663b37052958d9ff46b6
jeffreythomas <jeffreykthomas@gmail.com>
2023-02-27 21:41:50 -0800

    Reorganized file structure, new README.md

--- README.md
+++
@@ -1,7 +0,0 @@
-# GeoLife-Trajectories
-Final Project Data Analytics and IoT
-Our goal is to build a model that can use this data set https://www.microsoft.com/en-us/download/details.aspx?id=52367
-and predict the time forecasted in a location and tell us the number of devices available in that location, which means the traffic at that proximity location.
-The first part is extracting data from the dataset since creating the CSV file would be over 2.5 GB we decided to let the user download the dataset 
-and just run our code to obtain their dataset.
-also we have saved our model and you can just load our trained model to try it youself. instructions are given in the jupyter notebook.
--- data_presentation/generate_tableau_csv.py
+++ data_presentation/generate_tableau_csv.py
@@ -2,9 +2,9 @@ import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
 
-from prepare_data import prepare_data
-from transformer_classifier import build_model as transformer_build_model
-from lstm_time_series import build_model as lstm_build_model
+from data_cleaning.prepare_data import prepare_data
+from training_models.transformer_classifier import build_model as transformer_build_model
+from training_models.lstm_time_series import build_model as lstm_build_model
 
 
 def generate_tableau_csv():
@@ -20,25 +20,42 @@ def generate_tableau_csv():
     transformer_model.load_weights('transformer_model.h5')
 
     x_test_lstm = x_test[:, :4, :99]
-    x_test_transformer = x_test[:, :, :]
+    for idx in range(0, 10):
+        x_test_pred = x_test_lstm[:, :, idx:(99 + idx)]
+        y_pred = lstm_model.predict(x_test_pred)
+        x_test_lstm = np.concatenate((x_test_lstm, y_pred[..., None]), axis=2)
 
-    y_pred_location = lstm_model.predict(x_test_lstm)
+    y_pred_location = x_test_lstm[:, :, 99:109]
+    zeros = np.zeros((7436, 4, 90))
+    y = np.concatenate((zeros, y_pred_location), axis=2)
+
+    x_test_transformer = x_test[:, :, :]
     y_pred_transportation_mode = transformer_model.predict(x_test_transformer)
 
-    # replace last location data in each of the test sequences with the lstm prediction
-    x_test[:, :4, 99] = y_pred_location
-
-    for i in range(x_test.shape[1]):
+    for i in range(y.shape[1]):
         x_test[:, i, :] = scalers[i].inverse_transform(x_test[:, i, :])
+        y[:, i, :] = scalers[i].inverse_transform(y[:, i, :])
 
     label_dict = {'walk': 0, 'motor vehicle': 1, 'bike': 2, 'train': 3}
     inv_map = {v: k for k, v in label_dict.items()}
     y_pred_transformer = np.argmax(y_pred_transportation_mode, axis=1)
     y_pred_transformer = np.array([inv_map.get(item, item) for item in y_pred_transformer])
 
+    # find best estimate to display
+    idx = 0
+    diff = 1000
+    x_compare = x_test[:, :3, 99]
+    y_compare = y[:, :3, 90]
+    for n in range(x_compare.shape[0]):
+        if np.linalg.norm(y_compare[n, :] - x_compare[n, :]) < diff:
+            idx = n
+            diff = np.linalg.norm(y_compare[n, :] - x_compare[n, :])
+    x_test = np.concatenate((x_test[:, :4, :99], y[:, :, 90:]), axis=2)
+
     # select random sequence to display
-    rand_index = np.random.randint(0, len(x_test))
-    df_tableau = pd.DataFrame(x_test[rand_index, :, :].transpose(), columns=['x', 'y', 'z', 'timedelta', 'distance'])
+    # rand_index = np.random.randint(0, len(x_test))
+    rand_index = idx
+    df_tableau = pd.DataFrame(x_test[rand_index, :, :].transpose(), columns=['x', 'y', 'z', 'timedelta'])
     df_tableau['mode_prediction'] = y_pred_transformer[rand_index]
 
     R = 6371
@@ -47,8 +64,9 @@ def generate_tableau_csv():
 
     df_tableau.drop(['x', 'y', 'z'], axis=1, inplace=True)
     df_tableau['timestamp'] = df_tableau.apply(lambda x: datetime.strptime('2007-08-04 03:30:32', '%Y-%m-%d %H:%M:%S') + timedelta(seconds=x['timedelta']), axis=1)
+    df_tableau['time_diff'] = (df_tableau['timedelta']-df_tableau['timedelta'].shift()).fillna(0)
 
-    df_tableau.to_csv('tableau_data.csv')
+    df_tableau.to_csv('results/tableau_data.csv')
 
 
 if __name__ == '__main__':
